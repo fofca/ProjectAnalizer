03fecfef762f3debc9a30edf810616cafb01d493
Nilolay <Nilolay@DESKTOP-H3G20SO>
2023-04-17 15:23:40 +0300

    Добавьте файлы проекта.

---
+++ ProjectAnalizer.Core/Class1.cs
@@ -0,0 +1,110 @@
+﻿namespace ProjectAnalizer.Core
+{
+    public static class Class1
+    {
+
+        public static string AllFiles(this String str)
+
+        {
+            try
+            {
+                int info = 0;
+
+                DirectoryInfo di = new(str);
+
+                foreach (var fi in di.GetFiles("*.*", SearchOption.AllDirectories))
+                {
+                    info++;
+
+                }
+                return "Кол-во файлов: " + info.ToString();
+            }
+            catch (Exception)
+            {
+                return "Неверно введен путь к папке";
+            }
+
+        }
+        public static string InterfaceFiles(this String str)
+        {
+            try
+            {
+                int info = 0;
+                DirectoryInfo di = new(str);
+                string s = "";
+                foreach (var fi in di.GetFiles("*.cs", SearchOption.AllDirectories))
+                {
+                    s = File.ReadAllText(fi.Directory + @"\" + fi.Name);
+                    if (s.Contains(" internal interface ") == true)
+                    {
+                        info += 1;
+                    }
+
+
+                }
+                return "Кол-во фйлов интерфейсов: " + info.ToString();
+            }
+            catch (Exception)
+            {
+
+                return "Неверно введен путь к папке";
+            }
+
+        }
+        public static string ClassFiles(this String str)
+        {
+            try
+            {
+                int info = 0;
+
+                DirectoryInfo di = new(str);
+                string s = "";
+
+                foreach (var fi in di.GetFiles("*.cs", SearchOption.AllDirectories))
+                {
+                    s = File.ReadAllText(fi.Directory + @"\" + fi.Name);
+                    if (s.Contains(" class ") == true)
+                    {
+                        info += 1;
+                    }
+
+
+                }
+                return "Кол-во файлов классов: " + info.ToString();
+            }
+            catch (Exception)
+            {
+                return "Неверно введен путь к папке";
+            }
+
+        }
+        public static string EnumFiles(this String str)
+        {
+            try
+            {
+                int info = 0;
+
+                DirectoryInfo di = new(str);
+                string s = "";
+
+                foreach (var fi in di.GetFiles("*.cs", SearchOption.AllDirectories))
+                {
+                    s = File.ReadAllText(fi.Directory + @"\" + fi.Name);
+                    if (s.Contains(" enum ") == true)
+                    {
+                        info += 1;
+                    }
+
+
+                }
+                return "Кол-во файлов enum: " + info.ToString();
+            }
+            catch (Exception)
+            {
+
+                return "Неверно введен путь к папке";
+            }
+
+        }
+    }
+}
---
+++ ProjectAnalizer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        string command = GetCommandLineArugments(args);
+        string fileLocation = Assembly.GetExecutingAssembly().Location;
+        fileLocation = Path.GetDirectoryName(fileLocation);
+        fileLocation = fileLocation.Replace("\\ProjectAnalizer\\bin\\Release\\net6.0\\publish", string.Empty);
+        fileLocation += "\\ProjectAnalizer.Cli\\bin\\Release\\net6.0\\publish\\ProjectAnalizer.Cli.exe";
+
+        System.Diagnostics.Process process = new System.Diagnostics.Process();
+        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+        startInfo.FileName = "cmd.exe";
+        startInfo.Arguments = $"/c {fileLocation} {command}";
+        process.StartInfo = startInfo;
+        process.Start();
+    }
+    public static string GetCommandLineArugments(string[] args)
+    {
+        string retVal = string.Empty;
+
+        foreach (string arg in args)
+            retVal += arg;
+
+
+        return retVal;
+    }
+}
